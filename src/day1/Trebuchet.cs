2ba671a5745e72d72c8f3a43545ee651c645aaf2
Daniele Megna <daniele.megna@qmates.tech>
2023-12-04 00:48:57 +0100

    Raw solution for day 1 provided sample

--- src/day1/Trebuchet.cs
+++ src/day1/Trebuchet.cs
@@ -4,6 +4,17 @@ public class Trebuchet
 {
   public int SumOfCalibrationValuesFor(string input)
   {
-    return 142;
+    string[] lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+    return lines.Select(CalibrationValueFromRow).Sum();
+  }
+
+  private int CalibrationValueFromRow(string row)
+  {
+    var onlyNumbersArray = row
+      .ToCharArray()
+      .Select((c) => c.ToString())
+      .Where((c) => int.TryParse(c, out _))
+      .ToArray();
+    return int.Parse(onlyNumbersArray.First() + onlyNumbersArray.Last());
   }
 }
