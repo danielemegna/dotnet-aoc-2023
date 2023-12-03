83cef9ae98263c14e8a9ded3f14b7e1524368319
Daniele Megna <daniele.megna@qmates.tech>
2023-12-04 00:10:22 +0100

    Extracted Trebuchet class renaming Class1 one

---
+++ src/day1/Trebuchet.cs
@@ -0,0 +1,9 @@
+namespace aoc2023;
+
+public class Trebuchet
+{
+  public int SumOfCalibrationValuesFor(string input)
+  {
+    return 142;
+  }
+}
---
+++ test/day1/TrebuchetTest.cs
@@ -0,0 +1,22 @@
+﻿namespace aoc2023;
+
+using Xunit;
+
+public class TrebuchetTest
+{
+
+  [Fact]
+  public void SolveTheProvidedExample()
+  {
+    var input = "1abc2\n" +
+      "pqr3stu8vwx\n" +
+      "a1b2c3d4e5f\n" +
+      "treb7uchet\n";
+    var trebuchet = new Trebuchet();
+
+    var actual = trebuchet.SumOfCalibrationValuesFor(input);
+
+    Assert.Equal(142, actual);
+  }
+
+}
