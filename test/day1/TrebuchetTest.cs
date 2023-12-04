3fd129fab66bfa3e3069c226f092f917822779f7
Daniele Megna <daniele.megna@qmates.tech>
2023-12-04 08:39:58 +0100

    Add skipped test for second part

--- test/day1/TrebuchetTest.cs
+++ test/day1/TrebuchetTest.cs
@@ -18,7 +18,7 @@ public class TrebuchetTest
 
     var actual = solver.SumOfCalibrationValuesFor(input);
 
-    Assert.Equal(142, actual);
+    Assert.Equal(12 + 38 + 15 + 77, actual);
   }
 
   [Fact]
@@ -28,4 +28,23 @@ public class TrebuchetTest
     var actual = solver.SumOfCalibrationValuesFor(input);
     Assert.Equal(54388, actual);
   }
+
+  [Fact(Skip = "WIP")]
+  public void SolveProvidedExampleWithSpelledNumbers()
+  {
+    var input = new string[] {
+      "two1nine",
+      "eightwothree",
+      "abcone2threexyz",
+      "xtwone3four",
+      "4nineeightseven2",
+      "zoneight234",
+      "7pqrstsixteen"
+    };
+
+    var actual = solver.SumOfCalibrationValuesFor(input);
+
+    Assert.Equal(29 + 83 + 13 + 24 + 42 + 14 + 76, actual);
+  }
+
 }
