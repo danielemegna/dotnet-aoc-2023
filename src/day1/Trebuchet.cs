adb99f53719ff0f027ccf9732228d93146acb8c7
Daniele Megna <daniele.megna@qmates.tech>
2023-12-05 19:54:27 +0100

    Refactoring in Trebuchet final solution

--- src/day1/Trebuchet.cs
+++ src/day1/Trebuchet.cs
@@ -5,6 +5,8 @@ public class Trebuchet
 {
   public enum Mode { ONLY_NUMBERS, NUMBERS_AND_SPELLED }
 
+  private readonly string[] SORTED_SUPPORTED_WORDS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
   public int SumOfCalibrationValuesFor(string[] inputLines, Mode mode)
   {
     return inputLines.Select((line) => CalibrationValueFromRow(line, mode)).Sum();
@@ -12,40 +14,49 @@ public class Trebuchet
 
   public int CalibrationValueFromRow(string inputLine, Mode mode)
   {
-    string firstNumber = FirstNumberIn(inputLine, mode, false);
-    string lastNumber = FirstNumberIn(inputLine, mode, true);
+    string firstNumber = FirstNumberIn(inputLine, mode);
+    string lastNumber = LastNumberIn(inputLine, mode);
     return int.Parse(firstNumber + lastNumber);
   }
 
-  private string FirstNumberIn(string input, Mode mode, bool reversed)
+  private string FirstNumberIn(string input, Mode mode)
   {
-    var wordsDictionary = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-    if (reversed)
-    {
-      input = Reverse(input);
-      wordsDictionary = wordsDictionary.Select(Reverse).ToArray();
-    }
+    return FirstNumberUsing(input, SORTED_SUPPORTED_WORDS, mode);
+  }
 
+  private string LastNumberIn(string input, Mode mode)
+  {
+    var reversedInput = Reverse(input);
+    var reversedSortedSupportedWords = SORTED_SUPPORTED_WORDS.Select(Reverse).ToArray();
+    return FirstNumberUsing(reversedInput, reversedSortedSupportedWords, mode);
+  }
+
+  private string FirstNumberUsing(string input, string[] sortedSupportedWords, Mode mode)
+  {
     for (int inputCursor = 0; inputCursor < input.Length; inputCursor++)
     {
       char c = input[inputCursor];
-      if (int.TryParse(c.ToString(), out _))
+      if (isAnInteger(c))
         return c.ToString();
 
       if (mode == Mode.ONLY_NUMBERS)
         continue;
 
       var substring = input.Substring(0, inputCursor + 1);
-      for (int wordsCursor = 0; wordsCursor < wordsDictionary.Length; wordsCursor++)
+      for (int wordsCursor = 0; wordsCursor < sortedSupportedWords.Length; wordsCursor++)
       {
-        var currentWord = wordsDictionary[wordsCursor];
+        var currentWord = sortedSupportedWords[wordsCursor];
         if (substring.Contains(currentWord))
           return (wordsCursor + 1).ToString();
       }
-
     }
 
     throw new Exception("Cannot find number in: " + input);
+  }
+
+  private bool isAnInteger(char c)
+  {
+    return int.TryParse(c.ToString(), out _);
   }
 
   private string Reverse(string value)
