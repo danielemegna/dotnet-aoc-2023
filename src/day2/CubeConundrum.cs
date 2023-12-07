b20e98eae41a260281a9a7dc34ca67c3f75f4404
Daniele Megna <daniele.megna@qmates.tech>
2023-12-07 10:26:11 +0100

    Handle parse + get max count in sets for cube color
    Lot of code to parse the input game, set, cubes .... to be refactored

--- src/day2/CubeConundrum.cs
+++ src/day2/CubeConundrum.cs
@@ -13,23 +13,86 @@ public class CubeConundrum
   {
     return input.Select(line =>
     {
-      Regex regex = new Regex(@"^Game (\d):");
+      Regex regex = new Regex(@"^Game (\d): (.+$)");
       MatchCollection matchCollection = regex.Matches(line);
-      if(matchCollection.Count < 1 || matchCollection[0].Groups.Count < 2)
+
+      if (matchCollection.Count < 1 || matchCollection[0].Groups.Count < (1 + 2))
         throw new FormatException("Cannot parse game line: " + line);
 
       int gameId = int.Parse(matchCollection[0].Groups[1].Value);
-      return new Game(gameId);
+      string setsString = matchCollection[0].Groups[2].Value;
+
+      var game = new Game(gameId);
+
+      string[] setStringsArray = setsString.Split(";", StringSplitOptions.TrimEntries);
+      foreach (var singleSetString in setStringsArray)
+      {
+        var gameSet = new Game.Set();
+        string[] singleSetParts = singleSetString.Split(",", StringSplitOptions.TrimEntries);
+        foreach (var cubeData in singleSetParts)
+        {
+          string[] parts = cubeData.Split(" ", StringSplitOptions.TrimEntries);
+          int quantity = int.Parse(parts[0]);
+          CubeColor cubeColor = (CubeColor)Enum.Parse(typeof(CubeColor), parts[1].ToUpper());
+          gameSet.AddCubeCount(cubeColor, quantity);
+        }
+        game.AddSet(gameSet);
+      }
+      return game;
     }).ToArray();
   }
 }
 
-public class Game
+public enum CubeColor { BLUE, RED, GREEN };
+
+public class Game(int id)
 {
-  public Game(int id)
+  public int Id { get; } = id;
+  private int MaxBlueCubesCount = 0;
+  private int MaxRedCubesCount = 0;
+  private int MaxGreenCubesCount = 0;
+
+  public void AddSet(Set gameSet)
   {
-    this.Id = id;
+    if (MaxBlueCubesCount < gameSet.BlueCubesCount)
+      MaxBlueCubesCount = gameSet.BlueCubesCount;
+    if (MaxRedCubesCount < gameSet.RedCubesCount)
+      MaxRedCubesCount = gameSet.RedCubesCount;
+    if (MaxGreenCubesCount < gameSet.GreenCubesCount)
+      MaxGreenCubesCount = gameSet.GreenCubesCount;
   }
 
-  public int Id { get; }
+  public int MaxCountInSet(CubeColor cubeColor)
+  {
+    return cubeColor switch
+    {
+      CubeColor.BLUE => MaxBlueCubesCount,
+      CubeColor.RED => MaxRedCubesCount,
+      CubeColor.GREEN => MaxGreenCubesCount,
+    };
+  }
+
+  public class Set()
+  {
+    public int BlueCubesCount { get; private set; } = 0;
+    public int RedCubesCount { get; private set; } = 0;
+    public int GreenCubesCount { get; private set; } = 0;
+
+    public void AddCubeCount(CubeColor color, int count)
+    {
+      switch (color)
+      {
+        case CubeColor.BLUE:
+          BlueCubesCount = count;
+          break;
+        case CubeColor.RED:
+          RedCubesCount = count;
+          break;
+        case CubeColor.GREEN:
+          GreenCubesCount = count;
+          break;
+      }
+    }
+  }
+
 }
