0a8ea715f01dd7702e1d167286455f7492f23a38
Daniele Megna <daniele.megna@qmates.tech>
2023-12-07 11:11:53 +0100

    Some extract method refactor in GameParser

--- src/day2/GameParser.cs
+++ src/day2/GameParser.cs
@@ -4,7 +4,6 @@ namespace aoc2023.day2;
 
 class GameParser()
 {
-
   public static Game ParseGame(string gameInputLine)
   {
     Regex regex = new Regex(@"^Game (\d): (.+$)");
@@ -16,23 +15,37 @@ class GameParser()
     int gameId = int.Parse(matchCollection[0].Groups[1].Value);
     string setsString = matchCollection[0].Groups[2].Value;
 
-    var game = new Game(gameId);
+    return BuildGameWith(gameId, setsString);
+  }
 
+  private static Game BuildGameWith(int gameId, string setsString)
+  {
     string[] setStringsArray = setsString.Split(";", StringSplitOptions.TrimEntries);
+
+    var game = new Game(gameId);
     foreach (var singleSetString in setStringsArray)
     {
-      var gameSet = new Game.Set();
-      string[] singleSetParts = singleSetString.Split(",", StringSplitOptions.TrimEntries);
-      foreach (var cubeData in singleSetParts)
-      {
-        string[] parts = cubeData.Split(" ", StringSplitOptions.TrimEntries);
-        int quantity = int.Parse(parts[0]);
-        CubeColor cubeColor = (CubeColor)Enum.Parse(typeof(CubeColor), parts[1].ToUpper());
-        gameSet.AddCubeCount(cubeColor, quantity);
-      }
+      var gameSet = ParseSet(singleSetString);
       game.AddSet(gameSet);
     }
+
     return game;
+  }
+
+  private static Game.Set ParseSet(string setString)
+  {
+    var gameSet = new Game.Set();
+
+    string[] singleSetParts = setString.Split(",", StringSplitOptions.TrimEntries);
+    foreach (var cubeData in singleSetParts)
+    {
+      string[] parts = cubeData.Split(" ", StringSplitOptions.TrimEntries);
+      int quantity = int.Parse(parts[0]);
+      CubeColor cubeColor = (CubeColor)Enum.Parse(typeof(CubeColor), parts[1].ToUpper());
+      gameSet.AddCubeCount(cubeColor, quantity);
+    }
+
+    return gameSet;
   }
 
 
