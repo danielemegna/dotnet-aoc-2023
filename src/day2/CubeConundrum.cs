9116d6615462af12a3b8a2d5098df4afe6fe0b76
Daniele Megna <daniele.megna@qmates.tech>
2023-12-07 11:21:17 +0100

    First part of day 2 solved ! :tada:

--- src/day2/CubeConundrum.cs
+++ src/day2/CubeConundrum.cs
@@ -4,11 +4,23 @@ public class CubeConundrum
 {
   public int SumOfPossibileGame(string[] inputLines)
   {
-    return 1 + 2 + 5;
+    var MaxPossibleRedCubesCount = 12;
+    var MaxPossibleGreenCubesCount = 13;
+    var MaxPossibleBlueCubesCount = 14;
+
+    var games = ParseGames(inputLines);
+    return games.Where(game =>
+    {
+      return
+        game.MaxCountInSet(CubeColor.RED) <= MaxPossibleRedCubesCount &&
+        game.MaxCountInSet(CubeColor.GREEN) <= MaxPossibleGreenCubesCount &&
+        game.MaxCountInSet(CubeColor.BLUE) <= MaxPossibleBlueCubesCount;
+    })
+    .Sum(game => game.Id);
   }
 
-  internal Game[] ParseGames(string[] input)
+  internal Game[] ParseGames(string[] inputLines)
   {
-    return input.Select(GameParser.ParseGame).ToArray();
+    return inputLines.Select(GameParser.ParseGame).ToArray();
   }
 }
