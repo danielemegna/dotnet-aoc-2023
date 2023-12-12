d10fc2a8908391250216ce89ecc256a01c85c46c
Daniele Megna <daniele.megna@qmates.tech>
2023-12-12 08:11:30 +0100

    Add GearRatios concept and failing test in EngineSchematicParserTest

--- test/day3/EngineSchematicParserTest.cs
+++ test/day3/EngineSchematicParserTest.cs
@@ -8,34 +8,47 @@ public class EngineSchematicParserTest
   public class ParseProvidedExample()
   {
     [Fact]
-    public void GetEngineParts()
+    public void AndGetEngineParts()
     {
       var actual = EngineSchematicParser.Parse(GearRatiosTest.PROVIDED_EXAMPLE_INPUT_LINES);
       Assert.Equal<int[]>([467, 35, 633, 617, 592, 755, 664, 598], actual.EngineParts);
     }
+
+    [Fact(Skip = "WIP")]
+    public void AndGetGearRatios()
+    {
+      var actual = EngineSchematicParser.Parse(GearRatiosTest.PROVIDED_EXAMPLE_INPUT_LINES);
+      Assert.Equal<int[]>([(467 * 35), (755 * 598)], actual.GearRatios);
+    }
   }
 
-  public class ParseAnotherExample() {
-    
+  public class ParseAnotherExampleWithPartsAtTheEdgeButWithoutGears()
+  {
+    private readonly string[] inputLines = [
+      ".....739.....",
+      "11....@..92..",
+      ".............",
+      "......83.....",
+      "44...........",
+      ".#.........32",
+      "...133....*..",
+      ".............",
+      "....&........",
+      ".....82......",
+    ];
+
     [Fact]
-    public void GetEngineParts()
+    public void AndGetEngineParts()
     {
-      string[] input = [
-        ".....739.....",
-        "11....@..92..",
-        ".............",
-        "......83.....",
-        "44...........",
-        ".#.........32",
-        "...133....*..",
-        ".............",
-        "....&........",
-        ".....82......",
-      ];
-
-      var actual = EngineSchematicParser.Parse(input);
-
+      var actual = EngineSchematicParser.Parse(inputLines);
       Assert.Equal<int[]>([739, 44, 32, 82], actual.EngineParts);
+    }
+
+    [Fact]
+    public void AndGetGearRatios()
+    {
+      var actual = EngineSchematicParser.Parse(inputLines);
+      Assert.Equal<int[]>([], actual.GearRatios);
     }
   }
 }
