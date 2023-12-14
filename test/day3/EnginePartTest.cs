058534471df025047181a8eab65318f8c856c4a7
Daniele Megna <daniele.megna@qmates.tech>
2023-12-14 07:39:25 +0100

    Handle adjacent numbers below

--- test/day3/EnginePartTest.cs
+++ test/day3/EnginePartTest.cs
@@ -226,4 +226,129 @@ public class EnginePartTest
       Assert.Equal<int[]>([56, 227], actual.AdjacentNumbers);
     }
   }
+
+  public class BelowAdjacentNumbers
+  {
+
+    [Fact]
+    public void WithOne()
+    {
+      string[] inputMatrix = [
+        ".....",
+        "..*..",
+        "..64.",
+      ];
+
+      var actual = EnginePart.From(inputMatrix, new(2, 1));
+
+      Assert.Equal('*', actual.Symbol);
+      Assert.Equal<int[]>([64], actual.AdjacentNumbers);
+    }
+
+    [Fact]
+    public void WithOneStartingBefore()
+    {
+      string[] inputMatrix = [
+        ".....",
+        "..*..",
+        ".23..",
+      ];
+
+      var actual = EnginePart.From(inputMatrix, new(2, 1));
+
+      Assert.Equal('*', actual.Symbol);
+      Assert.Equal<int[]>([23], actual.AdjacentNumbers);
+    }
+
+    [Fact]
+    public void WithOneStartingFarBefore()
+    {
+      string[] inputMatrix = [
+        "......",
+        "...*..",
+        ".68...",
+      ];
+
+      var actual = EnginePart.From(inputMatrix, new(3, 1));
+
+      Assert.Equal('*', actual.Symbol);
+      Assert.Equal<int[]>([68], actual.AdjacentNumbers);
+    }
+
+
+    [Fact]
+    public void WithOneStartingAfter()
+    {
+      string[] inputMatrix = [
+        "......",
+        "..*...",
+        "...18.",
+      ];
+
+      var actual = EnginePart.From(inputMatrix, new(2, 1));
+
+      Assert.Equal('*', actual.Symbol);
+      Assert.Equal<int[]>([18], actual.AdjacentNumbers);
+    }
+
+    [Fact]
+    public void WithOneBig()
+    {
+      string[] inputMatrix = [
+        "........",
+        "...*....",
+        ".316935.",
+      ];
+
+      var actual = EnginePart.From(inputMatrix, new(3, 1));
+
+      Assert.Equal('*', actual.Symbol);
+      Assert.Equal<int[]>([316935], actual.AdjacentNumbers);
+    }
+
+    [Fact]
+    public void WithOneInLeftCorner()
+    {
+      string[] inputMatrix = [
+        ".....",
+        "..*..",
+        "43...",
+      ];
+
+      var actual = EnginePart.From(inputMatrix, new(2, 1));
+
+      Assert.Equal('*', actual.Symbol);
+      Assert.Equal<int[]>([43], actual.AdjacentNumbers);
+    }
+
+    [Fact]
+    public void WithOneInRightCorner()
+    {
+      string[] inputMatrix = [
+        ".....",
+        "..*..",
+        "...74",
+      ];
+
+      var actual = EnginePart.From(inputMatrix, new(2, 1));
+
+      Assert.Equal('*', actual.Symbol);
+      Assert.Equal<int[]>([74], actual.AdjacentNumbers);
+    }
+
+    [Fact]
+    public void WithMultiple()
+    {
+      string[] inputMatrix = [
+        "........",
+        "....*...",
+        ".313.79.",
+      ];
+
+      var actual = EnginePart.From(inputMatrix, new(4, 1));
+
+      Assert.Equal('*', actual.Symbol);
+      Assert.Equal<int[]>([313, 79], actual.AdjacentNumbers);
+    }
+  }
 }
