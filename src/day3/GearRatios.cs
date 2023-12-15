15307332dafa10c8a8e0b17e58b154d4e607cbce
Daniele Megna <daniele.megna@qmates.tech>
2023-12-15 14:58:14 +0100

    Replace use of EngineSchematicParser in GearRatios
    just move the EnginePart.Parse loop in a
    GearRatios.ParseEngineSchematic internal method

--- src/day3/GearRatios.cs
+++ src/day3/GearRatios.cs
@@ -5,14 +5,37 @@ public class GearRatios
 {
   public int SumEngineParts(string[] inputLines)
   {
-    var engineSchematic = EngineSchematicParser.Parse(inputLines);
-    return engineSchematic.EngineParts.Sum();
+    var engineParts = ParseEngineSchematic(inputLines);
+    return engineParts
+      .Select(e => e.AdjacentNumbers.Sum())
+      .Sum();
   }
 
   public int SumGearRatios(string[] inputLines)
   {
-    var engineSchematic = EngineSchematicParser.Parse(inputLines);
-    return engineSchematic.GearRatios.Sum();
+    var engineParts = ParseEngineSchematic(inputLines);
+    return engineParts
+      .Where(e => e.IsAGear())
+      .Select(e => e.GearRatio())
+      .Sum();
+  }
+
+  internal EnginePart[] ParseEngineSchematic(string[] inputLines)
+  {
+    List<EnginePart> result = [];
+    for (int y = 0; y < inputLines.Length; y++)
+    {
+      for (int x = 0; x < inputLines[y].Length; x++)
+      {
+        char currentChar = inputLines[y][x];
+        if (EnginePart.IsAnInteger(currentChar) || currentChar == EnginePart.DOT_CHAR_CODE)
+          continue;
+
+        EnginePart enginePart = EnginePart.From(inputLines, new(x, y));
+        result.Add(enginePart);
+      }
+    }
+    return result.ToArray();
   }
 
 }
