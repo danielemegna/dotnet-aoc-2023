5a0ff6470d4d4317b587396fccc19e1d8fd3a1e5
Daniele Megna <daniele.megna@qmates.tech>
2023-12-15 08:49:52 +0100

    Good refactor in EnginePart.From

--- src/day3/EnginePart.cs
+++ src/day3/EnginePart.cs
@@ -10,64 +10,90 @@ public class EnginePart(char symbol, int[] adjacentNumbers)
   public char Symbol { get; } = symbol;
   public int[] AdjacentNumbers { get; } = adjacentNumbers;
 
-  public static EnginePart From(string[] inputMatrix, Coordinate coordinate)
+  public static EnginePart From(string[] inputMatrix, Coordinate partCoordinate)
   {
-    (int x, int y) = coordinate;
+    (int x, int y) = partCoordinate;
     char symbol = inputMatrix[y][x];
 
-    int[] numbersOnTheRight = NumbersOnTheRight(inputMatrix, coordinate);
-    int[] numbersOnTheLeft = NumbersOnTheLeft(inputMatrix, coordinate);
-    int[] numbersAbove = NumbersAbove(inputMatrix, coordinate);
-    int[] numbersBelow = NumbersBelow(inputMatrix, coordinate);
+    int[] numbersOnTheRight = NumbersOnTheRight(inputMatrix, partCoordinate);
+    int[] numbersOnTheLeft = NumbersOnTheLeft(inputMatrix, partCoordinate);
+    int[] numbersAbove = NumbersAbove(inputMatrix, partCoordinate);
+    int[] numbersBelow = NumbersBelow(inputMatrix, partCoordinate);
 
     return new EnginePart(symbol, [.. numbersOnTheRight, .. numbersOnTheLeft, .. numbersAbove, .. numbersBelow]);
   }
 
-  private static int[] NumbersOnTheRight(string[] inputMatrix, Coordinate partCoordinate)
-  {
-    (int partX, int partY) = partCoordinate;
-    int maxPossibileXValue = inputMatrix[partY].Length - 1;
-    int rightX = Math.Min(partX + 1, maxPossibileXValue);
-    return FindNumbersWith(inputMatrix, partY, rightX, rightX, maxPossibileXValue);
-  }
-
   private static int[] NumbersOnTheLeft(string[] inputMatrix, Coordinate partCoordinate)
   {
-    (int partX, int partY) = partCoordinate;
-    int leftX = Math.Max(0, partX - 1);
-    return FindNumbersWith(inputMatrix, partY, leftX, leftX, partX);
+    (int x, int y) = partCoordinate;
+    if (x == 0)
+      return [];
+
+    int leftX = x - 1;
+    return FindNumbersWith(
+      inputMatrix: inputMatrix,
+      yCursor: y,
+      startingX: leftX,
+      endX: leftX,
+      maxPossibileXValue: leftX
+    );
+  }
+
+  private static int[] NumbersOnTheRight(string[] inputMatrix, Coordinate partCoordinate)
+  {
+    (int x, int y) = partCoordinate;
+    int maxPossibileXValue = inputMatrix[y].Length - 1;
+    if (x == maxPossibileXValue)
+      return [];
+
+    int rightX = x + 1;
+    return FindNumbersWith(
+      inputMatrix: inputMatrix,
+      yCursor: y,
+      startingX: rightX,
+      endX: rightX,
+      maxPossibileXValue: maxPossibileXValue
+    );
   }
 
   private static int[] NumbersAbove(string[] inputMatrix, Coordinate partCoordinate)
   {
-    (int partX, int partY) = partCoordinate;
-    if (partY == 0)
+    (int x, int y) = partCoordinate;
+    if (y == 0)
       return [];
 
-    int rowY = partY - 1;
-    return FindNumbersInRow(inputMatrix, rowY, partX);
+    int aboveRowY = y - 1;
+    int maxPossibileXValue = inputMatrix[aboveRowY].Length - 1;
+    return FindNumbersWith(
+      inputMatrix: inputMatrix,
+      yCursor: aboveRowY,
+      startingX: Math.Max(0, x - 1),
+      endX: Math.Min(x + 1, maxPossibileXValue),
+      maxPossibileXValue: maxPossibileXValue
+    );
   }
 
   private static int[] NumbersBelow(string[] inputMatrix, Coordinate partCoordinate)
   {
-    (int partX, int partY) = partCoordinate;
-    if (partY == (inputMatrix.Length - 1))
+    (int x, int y) = partCoordinate;
+    if (y == (inputMatrix.Length - 1))
       return [];
 
-    int rowY = partY + 1;
-    return FindNumbersInRow(inputMatrix, rowY, partX);
-  }
-
-  private static int[] FindNumbersInRow(string[] inputMatrix, int rowY, int partX)
-  {
-    int maxPossibileXValue = inputMatrix[rowY].Length - 1;
-    int startingX = Math.Max(0, partX - 1);
-    int endX = Math.Min(partX + 1, maxPossibileXValue);
-    return FindNumbersWith(inputMatrix, rowY, startingX, endX, maxPossibileXValue);
+    int belowRowY = y + 1;
+    int maxPossibileXValue = inputMatrix[belowRowY].Length - 1;
+    return FindNumbersWith(
+      inputMatrix: inputMatrix,
+      yCursor: belowRowY,
+      startingX: Math.Max(0, x - 1),
+      endX: Math.Min(x + 1, maxPossibileXValue),
+      maxPossibileXValue: maxPossibileXValue
+    );
   }
 
   private static int[] FindNumbersWith(string[] inputMatrix, int yCursor, int startingX, int endX, int maxPossibileXValue)
   {
+    // TODO here we could add a cache
+
     List<int> result = [];
     int xCursor = startingX;
 
