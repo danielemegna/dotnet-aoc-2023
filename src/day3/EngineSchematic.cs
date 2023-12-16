50d77720c5c5845e7107c30a44df330123575176
Daniele Megna <daniele.megna@qmates.tech>
2023-12-16 10:05:46 +0100

    Remove unused EngineSchematic class

--- src/day3/EngineSchematic.cs
+++
@@ -1,7 +0,0 @@
-namespace aoc2023.day3;
-
-public class EngineSchematic(int[] engineParts, int[] gearRatios)
-{
-    public int[] EngineParts { get; } = engineParts;
-    public int[] GearRatios { get; } = gearRatios;
-}
