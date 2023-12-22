2438435448ba75a054b470f74db0b2b782746518
Daniele Megna <daniele.megna@qmates.tech>
2023-12-22 07:29:36 +0100

    Skeleton for day 22 ...
    ...three-dimensionality and gravitational physics involved :scream:

---
+++ src/day22/Solver.cs
@@ -0,0 +1,9 @@
+namespace aoc2023.day22;
+
+public class Solver
+{
+  public int CountSafeToDisintegrateBricks(string[] inputLines)
+  {
+    throw new NotImplementedException();
+  }
+}
---
+++ test/day22/SolverTest.cs
@@ -0,0 +1,38 @@
+namespace aoc2023.day22;
+
+using Xunit;
+
+class SolverTest
+{
+  private static readonly string[] PROVIDED_EXAMPLE_INPUT_LINES = [
+    "1,0,1~1,2,1",
+    "0,0,2~2,0,2",
+    "0,2,3~2,2,3",
+    "0,0,4~0,2,4",
+    "2,0,5~2,2,5",
+    "0,1,6~2,1,6",
+    "1,1,8~1,1,9",
+  ];
+
+  private readonly Solver solver = new();
+
+  public class FirstPartTest : SolverTest
+  {
+
+    [Fact(Skip = "WIP")]
+    public void SolveTheProvidedExample()
+    {
+      var actual = solver.CountSafeToDisintegrateBricks(PROVIDED_EXAMPLE_INPUT_LINES);
+      Assert.Equal(5, actual);
+    }
+
+    [Fact(Skip = "WIP")]
+    public void SolveWithFile()
+    {
+      var input = File.ReadAllLines("day22/input.txt");
+      var actual = solver.CountSafeToDisintegrateBricks(input);
+      Assert.Equal(-1, actual);
+    }
+
+  }
+}
