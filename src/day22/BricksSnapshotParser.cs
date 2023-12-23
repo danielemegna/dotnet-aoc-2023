e32a1bd504abd16a7f65424fa2082bc41e484c90
Daniele Megna <daniele.megna@qmates.tech>
2023-12-23 08:01:42 +0100

    Parsing input lines into Bricks

--- src/day22/BricksSnapshotParser.cs
+++ src/day22/BricksSnapshotParser.cs
@@ -7,8 +7,24 @@ public class BricksSnapshotParser
     return new BricksSnapshot();
   }
 
-  internal Brick[] ParseBricksFrom(string[] inputLines)
+  public ISet<Brick> ParseBricks(string[] inputLines)
   {
-    throw new NotImplementedException();
+    return inputLines
+      .Select(inputLine =>
+      {
+        (Coordinate start, Coordinate end) = CoordinatesFrom(inputLine);
+        return new Brick(start, end);
+      }).ToHashSet();
+  }
+
+  private (Coordinate, Coordinate) CoordinatesFrom(string line)
+  {
+    var foo = line.Split("~").Select(coordinateString =>
+    {
+      var coordinateParts = coordinateString.Split(",").Select(n => int.Parse(n)).ToArray();
+      return new Coordinate(coordinateParts[0], coordinateParts[1], coordinateParts[2]);
+    });
+
+    return (foo.ElementAt(0), foo.ElementAt(1));
   }
 }
--- test/day22/BricksSnapshotParserTest.cs
+++ test/day22/BricksSnapshotParserTest.cs
@@ -8,6 +8,21 @@ public class BricksSnapshotParserTest
 
   public class ParseTheProvidedExample : BricksSnapshotParserTest
   {
+
+    [Fact]
+    public void ParseBricksList()
+    {
+      var bricks = parser.ParseBricks(SolverTest.PROVIDED_EXAMPLE_INPUT_LINES);
+
+      HashSet<Brick> expected = [
+        new Brick(new(1,0,1), new(1,2,1)), new Brick(new(0,0,2), new(2,0,2)),
+        new Brick(new(0,2,3), new(2,2,3)), new Brick(new(0,0,4), new(0,2,4)),
+        new Brick(new(2,0,5), new(2,2,5)), new Brick(new(0,1,6), new(2,1,6)),
+        new Brick(new(1,1,8), new(1,1,9)),
+      ];
+      Assert.Equivalent(expected, bricks);
+    }
+
     [Fact]
     public void GetBrickAtEmptyCoordinate()
     {
