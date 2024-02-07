400c6d6d5f1f4770dacda41059e10798f25c15e3
Daniele Megna <daniele.megna@qmates.tech>
2024-02-07 07:53:05 +0100

    Starting with Day 9 :flashlight:

---
+++ src/day9/Solver.cs
@@ -0,0 +1,9 @@
+namespace aoc2023.day9;
+
+public class Solver
+{
+  public int SumOfNextHistoryValues(string[] inputLines)
+  {
+    return 114;
+  }
+}
---
+++ test/day9/SolverTest.cs
@@ -0,0 +1,35 @@
+namespace aoc2023.day9;
+
+using Xunit;
+
+public class SolverTest
+{
+  public static readonly string[] PROVIDED_EXAMPLE_INPUT_LINES = [
+    "0 3 6 9 12 15",
+    "1 3 6 10 15 21",
+    "10 13 16 21 30 45",
+  ];
+
+  private readonly Solver solver = new();
+
+  public class FirstPartTest : SolverTest
+  {
+
+    [Fact]
+    public void SolveTheFirstProvidedExample()
+    {
+      var actual = solver.SumOfNextHistoryValues(PROVIDED_EXAMPLE_INPUT_LINES);
+      Assert.Equal(18 + 28 + 68, actual);
+    }
+
+    [Fact(Skip = "WIP")]
+    public void SolveWithFile()
+    {
+      var input = File.ReadAllLines("day9/input.txt");
+      var actual = solver.SumOfNextHistoryValues(input);
+      Assert.Equal(-1, actual);
+    }
+
+  }
+
+}
