f40514b2d2a47c280701b8537337d8816eabd834
Daniele Megna <daniele.megna@qmates.tech>
2024-02-28 07:47:25 +0100

    Use GardenMap.LoopLength in Solver

--- src/day10/Solver.cs
+++ src/day10/Solver.cs
@@ -2,41 +2,11 @@ namespace aoc2023.day10;
 
 class Solver
 {
-  private readonly ISet<Coordinate> visitedCoordinates = new HashSet<Coordinate>();
 
   public int FarthestPointFromStartDistance(string[] inputLines)
   {
     var gardenMap = GardenMap.From(inputLines);
-    int stepsCounter = 0;
-
-    var start = gardenMap.StartingPosition;
-    var (left, right) = gardenMap.ConnectionsFor(start);
-    visitedCoordinates.Add(start);
-    visitedCoordinates.Add(left);
-    visitedCoordinates.Add(right);
-    stepsCounter++;
-
-    while (left != right)
-    {
-      left = NotVisitedConnectionFor(left, gardenMap);
-      right = NotVisitedConnectionFor(right, gardenMap);
-
-      visitedCoordinates.Add(left);
-      visitedCoordinates.Add(right);
-      stepsCounter++;
-    }
-
-    return stepsCounter;
-  }
-
-  private Coordinate NotVisitedConnectionFor(Coordinate coordinate, GardenMap gardenMap)
-  {
-    var (left, right) = gardenMap.ConnectionsFor(coordinate);
-
-    if (visitedCoordinates.Contains(left))
-      return right;
-
-    return left;
+    return gardenMap.LoopLength / 2;
   }
 
 }
