298b82f5b1bab6e6ede59b633d812cba4b41be26
Nicola Moretto <moretto.nik@gmail.com>
2024-02-29 16:52:22 +0100

    Implemented sum of multiple paths from galaxies

--- src/day11/Cosmic.cs
+++ src/day11/Cosmic.cs
@@ -2,12 +2,28 @@ namespace aoc2023.day11;
 
 public class Cosmic
 {
+  private readonly Coordinate[] galaxies;
+
   public Cosmic(Coordinate[] galaxies)
   {
+    this.galaxies = galaxies;
   }
 
-  public int ShortestPathLengthBetweenCoordinates(Coordinate from, Coordinate to)
+  internal int ShortestPathLengthBetweenCoordinates(Coordinate from, Coordinate to)
   {
     return Math.Abs(to.X - from.X) + Math.Abs(to.Y - from.Y);
+  }
+
+  public int SumOfShortestPathsBetweenGalaxies()
+  {
+    int result = 0;
+    for (int i = 0; i < galaxies.Length; i++)
+    {
+      for (int j = i+1; j < galaxies.Length; j++)
+      {
+        result += ShortestPathLengthBetweenCoordinates(galaxies[i], galaxies[j]);
+      }
+    }
+    return result;
   }
 }
--- test/day11/CosmicTest.cs
+++ test/day11/CosmicTest.cs
@@ -21,4 +21,21 @@ public class CosmicTest
     Assert.Equal(9, actual);
   }
 
+  [Fact]
+  public void SumShortestPathForThreeGalaxies()
+  {
+    var cosmic = new Cosmic([new Coordinate(0, 0), new Coordinate(0, 5), new Coordinate(1, 9)]);
+    int actual = cosmic.SumOfShortestPathsBetweenGalaxies();
+    Assert.Equal(5 + 10 + 5, actual);
+  }
+
+  [Fact]
+  public void SumShortestPathForAdventExample()
+  {
+    var cosmic = new Cosmic([new Coordinate(0, 0), new Coordinate(0, 5), new Coordinate(1, 9),
+      new Coordinate(4, 12), new Coordinate(5, 1), new Coordinate(6, 8), new Coordinate(9, 0),
+      new Coordinate(10, 9), new Coordinate(11, 4)]);
+    int actual = cosmic.SumOfShortestPathsBetweenGalaxies();
+    Assert.Equal(374, actual);
+  }
 }
