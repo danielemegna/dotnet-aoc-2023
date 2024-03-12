4ebf60538532b2eaba22298589933a67c09e94b4
Daniele Megna <daniele.megna@qmates.tech>
2024-03-12 08:30:32 +0100

    First draft of IsCompatibleWithDamagedSpringsGroup
    - not optimized for sure
    - grouped together existing tests in PossibileArrangementsCount

--- src/day12/ConditionRecord.cs
+++ src/day12/ConditionRecord.cs
@@ -19,6 +19,33 @@ public class ConditionRecord
     return 1;
   }
 
+  internal bool IsCompatibleWithDamagedSpringsGroup(string springsStates)
+  {
+    var booleanSpringsStates = SpringStatesToBoolean(springsStates);
+
+    List<int> readDamagedSpringsGroups = [];
+    int currentDamagedSpringGroupValue = 0;
+    foreach (bool? state in booleanSpringsStates)
+    {
+      if (!state.HasValue)
+        throw new SystemException("Cannot check compatibility for uncompleted springs states array");
+
+      if (!state.Value)
+      {
+        currentDamagedSpringGroupValue++;
+        continue;
+      }
+
+      if(currentDamagedSpringGroupValue > 0) {
+        readDamagedSpringsGroups.Add(currentDamagedSpringGroupValue);
+        currentDamagedSpringGroupValue = 0;
+        // TODO optimize with break on incompatibility
+      }
+    }
+
+    return readDamagedSpringsGroups.ToArray() == this.damagedSpringsGroups;
+  }
+
   private static bool?[] SpringStatesToBoolean(string springsStates)
   {
     return springsStates
--- test/day12/ConditionRecordTest.cs
+++ test/day12/ConditionRecordTest.cs
@@ -4,42 +4,65 @@ using Xunit;
 
 public class ConditionRecordTest
 {
-
-  [Fact]
-  public void JustOnePossibileArrangements()
+  public class PossibileArrangementsCount
   {
-    var record = new ConditionRecord("???.###", [1, 1, 3]);
-    Assert.Equal(1, record.PossibileArrangementsCount());
+
+    [Fact]
+    public void JustOnePossibileArrangements()
+    {
+      var record = new ConditionRecord("???.###", [1, 1, 3]);
+      Assert.Equal(1, record.PossibileArrangementsCount());
+    }
+
+    [Fact(Skip = "WIP")]
+    public void FourPossibileArrangements()
+    {
+      var record = new ConditionRecord(".??..??...?##.", [1, 1, 3]);
+      Assert.Equal(4, record.PossibileArrangementsCount());
+    }
+
+    [Fact]
+    public void MoreComplexOnePossibileArrangements()
+    {
+      var record = new ConditionRecord("?#?#?#?#?#?#?#?", [1, 3, 1, 6]);
+      Assert.Equal(1, record.PossibileArrangementsCount());
+      record = new ConditionRecord("????.#...#...", [4, 1, 1]);
+      Assert.Equal(1, record.PossibileArrangementsCount());
+    }
+
+    [Fact(Skip = "WIP")]
+    public void MoreComplexFourPossibileArrangements()
+    {
+      var record = new ConditionRecord("????.######..#####.", [1, 6, 5]);
+      Assert.Equal(4, record.PossibileArrangementsCount());
+    }
+
+    [Fact(Skip = "WIP")]
+    public void TenPossibileArrangements()
+    {
+      var record = new ConditionRecord("?###????????", [3, 2, 1]);
+      Assert.Equal(10, record.PossibileArrangementsCount());
+    }
+
   }
 
-  [Fact(Skip = "WIP")]
-  public void FourPossibileArrangements()
-  {
-    var record = new ConditionRecord(".??..??...?##.", [1, 1, 3]);
-    Assert.Equal(4, record.PossibileArrangementsCount());
-  }
+  public class DamagedSpringsGroupCompatibilityCheck {
 
-  [Fact]
-  public void MoreComplexOnePossibileArrangements()
-  {
-    var record = new ConditionRecord("?#?#?#?#?#?#?#?", [1, 3, 1, 6]);
-    Assert.Equal(1, record.PossibileArrangementsCount());
-    record = new ConditionRecord("????.#...#...", [4, 1, 1]);
-    Assert.Equal(1, record.PossibileArrangementsCount());
-  }
+    [Fact]
+    public void NotCompatibleWithSimpleSingleGroup()
+    {
+      var record = new ConditionRecord("", [2]);
+      Assert.False(record.IsCompatibleWithDamagedSpringsGroup(".."));
+      Assert.False(record.IsCompatibleWithDamagedSpringsGroup("#."));
+      Assert.False(record.IsCompatibleWithDamagedSpringsGroup(".#"));
+    }
 
-  [Fact(Skip = "WIP")]
-  public void MoreComplexFourPossibileArrangements()
-  {
-    var record = new ConditionRecord("????.######..#####.", [1, 6, 5]);
-    Assert.Equal(4, record.PossibileArrangementsCount());
-  }
-
-  [Fact(Skip = "WIP")]
-  public void TenPossibileArrangements()
-  {
-    var record = new ConditionRecord("?###????????", [3, 2, 1]);
-    Assert.Equal(10, record.PossibileArrangementsCount());
+    [Fact]
+    public void CompatibleWithSimpleSingleGroup()
+    {
+      var record = new ConditionRecord("", [2]);
+      Assert.False(record.IsCompatibleWithDamagedSpringsGroup("##"));
+    }
   }
 
 }
