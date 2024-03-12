9682ea6a216ceea363ef69b4a50e3d34ae71ff36
Daniele Megna <daniele.megna@qmates.tech>
2024-03-12 07:48:06 +0100

    Convert ConditionRecord springsStates in array of booleans
    .. instead of keep them as array of chars

--- src/day12/ConditionRecord.cs
+++ src/day12/ConditionRecord.cs
@@ -3,15 +3,25 @@ namespace aoc2023.day12;
 
 public class ConditionRecord
 {
-  private readonly char[] springsStates;
+  private readonly bool?[] springsStates;
   private readonly int[] damagedSpringsGroups;
 
   public ConditionRecord(string springsStates, int[] damagedSpringsGroups)
+  : this(
+    springsStates
+      .ToCharArray()
+      .Select<char, bool?>(
+        c => c switch { '.' => true, '#' => false, _ => null }
+      ).ToArray(),
+    damagedSpringsGroups
+  )
+  { }
+
+  public ConditionRecord(bool?[] springsStates, int[] damagedSpringsGroups)
   {
-    this.springsStates = springsStates.ToCharArray();
+    this.springsStates = springsStates;
     this.damagedSpringsGroups = damagedSpringsGroups;
   }
-
 
   public int PossibileArrangementsCount()
   {
