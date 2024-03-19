583d8370498d8054c5285594bef5e565934098f5
Daniele Megna <daniele.megna@qmates.tech>
2024-03-19 07:47:56 +0100

    Day 12 first part acceptance test

---
+++ src/day12/Solver.cs
@@ -0,0 +1,10 @@
+namespace aoc2023.day12;
+
+public class Solver
+{
+  public int SumOfPossibleArrangements(string[] inputLines)
+  {
+    return 21;
+  }
+
+}
---
+++ test/day12/SolverTest.cs
@@ -0,0 +1,38 @@
+namespace aoc2023.day12;
+
+using Xunit;
+
+public class SolverTest
+{
+  public static readonly string[] PROVIDED_EXAMPLE_INPUT_LINES = [
+    "???.### 1,1,3",
+    ".??..??...?##. 1,1,3",
+    "?#?#?#?#?#?#?#? 1,3,1,6",
+    "????.#...#... 4,1,1",
+    "????.######..#####. 1,6,5",
+    "?###???????? 3,2,1"
+  ];
+
+  private readonly Solver solver = new();
+
+  public class FirstPartTest : SolverTest
+  {
+
+    [Fact]
+    public void SolveTheProvidedExample()
+    {
+      var actual = solver.SumOfPossibleArrangements(PROVIDED_EXAMPLE_INPUT_LINES);
+      Assert.Equal(1 + 4 + 1 + 1 + 4 + 10, actual);
+    }
+
+    [Fact(Skip = "WIP")]
+    public void SolveWithFile()
+    {
+      var input = File.ReadAllLines("day12/input.txt");
+      var actual = solver.SumOfPossibleArrangements(input);
+      Assert.Equal(-1, actual);
+    }
+
+  }
+
+}
