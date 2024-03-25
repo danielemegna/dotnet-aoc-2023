6d04a4333ecc5d6f49e63618ebb987a192f26ec1
Daniele Megna <daniele.megna@qmates.tech>
2024-03-25 07:38:03 +0100

    Day13: setup acceptance tests

---
+++ src/day13/Solver.cs
@@ -0,0 +1,9 @@
+namespace aoc2023.day13;
+
+class Solver
+{
+  public int SummarizePatternNotes(string[] inputLines)
+  {
+    throw new NotImplementedException();
+  }
+}
---
+++ test/day13/SolverTest.cs
@@ -0,0 +1,47 @@
+namespace aoc2023.day13;
+
+using Xunit;
+
+public class SolverTest
+{
+  public static readonly string[] PROVIDED_EXAMPLE_INPUT_LINES = [
+    "#.##..##.",
+    "..#.##.#.",
+    "##......#",
+    "##......#",
+    "..#.##.#.",
+    "..##..##.",
+    "#.#.##.#.",
+    "",
+    "#...##..#",
+    "#....#..#",
+    "..##..###",
+    "#####.##.",
+    "#####.##.",
+    "..##..###",
+    "#....#..#"
+  ];
+
+  private readonly Solver solver = new();
+
+  public class FirstPartTest : SolverTest
+  {
+
+    [Fact(Skip = "WIP")]
+    public void SolveTheProvidedExample()
+    {
+      var actual = solver.SummarizePatternNotes(PROVIDED_EXAMPLE_INPUT_LINES);
+      Assert.Equal(5 + (4 * 100), actual);
+    }
+
+    [Fact(Skip = "WIP")]
+    public void SolveWithFile()
+    {
+      var input = File.ReadAllLines("day13/input.txt");
+      var actual = solver.SummarizePatternNotes(input);
+      Assert.Equal(1232, actual);
+    }
+
+  }
+
+}
