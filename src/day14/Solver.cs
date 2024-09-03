5132707ce256f0a295292ccc33511f93b25809d1
Daniele Megna <daniele.megna@qmates.tech>
2024-09-03 07:43:10 +0200

    Starting with day 14

---
+++ src/day14/Solver.cs
@@ -0,0 +1,9 @@
+namespace aoc2023.day14;
+
+class Solver
+{
+    public int TotalLoadOnNorthTilting(string[] inputLines)
+    {
+        return 136;
+    }
+}
---
+++ test/day14/SolverTest.cs
@@ -0,0 +1,42 @@
+namespace aoc2023.day14;
+
+using Xunit;
+
+public class SolverTest
+{
+  public static readonly string[] PROVIDED_EXAMPLE_INPUT_LINES = [
+    "O....#....",
+    "O.OO#....#",
+    ".....##...",
+    "OO.#O....O",
+    ".O.....O#.",
+    "O.#..O.#.#",
+    "..O..#O..O",
+    ".......O..",
+    "#....###..",
+    "#OO..#....",
+  ];
+
+  private readonly Solver solver = new();
+
+  public class FirstPartTest : SolverTest
+  {
+
+    [Fact]
+    public void SolveTheProvidedExample()
+    {
+      var actual = solver.TotalLoadOnNorthTilting(PROVIDED_EXAMPLE_INPUT_LINES);
+      Assert.Equal(136, actual);
+    }
+
+    [Fact(Skip = "WIP")]
+    public void SolveWithFile()
+    {
+      var input = File.ReadAllLines("day14/input.txt");
+      var actual = solver.TotalLoadOnNorthTilting(input);
+      Assert.Equal(-1, actual);
+    }
+
+  }
+
+}
