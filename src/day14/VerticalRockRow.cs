8d42f57072a78cbd030145e6ddd43517dfe6856f
Daniele Megna <megna.dany@gmail.com>
2024-09-09 08:35:35 +0200

    Introducing new VerticalRockRow concept

---
+++ src/day14/VerticalRockRow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+
+namespace aoc2023.day14;
+
+class VerticalRockRow
+{
+    private MapObject[] rowObjects;
+
+    public VerticalRockRow(MapObject[] rowObjects)
+    {
+        this.rowObjects = rowObjects;
+    }
+
+    public override bool Equals(object? other)
+    {
+        if (this == other) return true;
+        if (other is null) return false;
+        if (other.GetType() != typeof(VerticalRockRow)) return false;
+        var otherCasted = (VerticalRockRow)other;
+
+        return StructuralComparisons.StructuralEqualityComparer.Equals(rowObjects, otherCasted.rowObjects);
+    }
+
+    public override int GetHashCode() =>
+      StructuralComparisons.StructuralEqualityComparer.GetHashCode(rowObjects);
+}
---
+++ test/day14/VerticalRockRowTest.cs
@@ -0,0 +1,28 @@
+namespace aoc2023.day14;
+
+using Xunit;
+
+public class VerticalRockRowTest
+{
+
+  [Fact]
+  public void Equality()
+  {
+    var row = new VerticalRockRow([MapObject.EMPTY_SPACE, MapObject.CUBE_ROCK, MapObject.ROUND_ROCK, MapObject.EMPTY_SPACE]);
+    var anotherRow = new VerticalRockRow([MapObject.ROUND_ROCK, MapObject.CUBE_ROCK, MapObject.EMPTY_SPACE, MapObject.ROUND_ROCK]);
+    var anotherCopyOfRow = new VerticalRockRow([MapObject.EMPTY_SPACE, MapObject.CUBE_ROCK, MapObject.ROUND_ROCK, MapObject.EMPTY_SPACE]);
+
+    Assert.Equal(row, row);
+    Assert.Same(row, row);
+
+    Assert.Equal(row, anotherCopyOfRow);
+    Assert.Equal(anotherCopyOfRow, row);
+    Assert.False(row == anotherCopyOfRow);
+    Assert.NotSame(row, anotherCopyOfRow);
+    Assert.Equal(row.GetHashCode(), anotherCopyOfRow.GetHashCode());
+
+    Assert.NotEqual(row, anotherRow);
+    Assert.NotEqual(anotherRow, anotherCopyOfRow);
+    Assert.NotEqual(row.GetHashCode(), anotherRow.GetHashCode());
+  }
+}
