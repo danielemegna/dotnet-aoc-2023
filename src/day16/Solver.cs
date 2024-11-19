759e1dc3233923832774f323b6f84edd2b67df1c
Daniele Megna <megna.dany@gmail.com>
2024-11-19 08:16:51 +0100

    Preparatory refactor to implement next Solver method
    extected common part that will be used to implement
    Solver.MaximumPossibileEnergizedTilesFor

--- src/day16/Solver.cs
+++ src/day16/Solver.cs
@@ -4,18 +4,26 @@ public class Solver
 {
   public int EnergizedTilesTotalCountFor(string[] inputLines)
   {
-    var contraptionMap = ContraptionMap.From(inputLines);
-
-    while(contraptionMap.GetExistingBeams().Count > 0) {
-      contraptionMap.MoveNextAllBeams();
-    }
-
-    return contraptionMap.EnergizedTilesCount();
+    Beam initialBeam = new Beam(
+        Coordinate: new Coordinate(X: 0, Y: 0),
+        Direction: BeamDirection.RIGHT
+    );
+    return EnergizedTilesTotalCountFor(inputLines, initialBeam);
   }
 
   public int MaximumPossibileEnergizedTilesFor(string[] inputLines)
   {
     throw new NotImplementedException();
+  }
+
+  private int EnergizedTilesTotalCountFor(string[] mapInputLines, Beam initialBeam)
+  {
+    var contraptionMap = ContraptionMap.From(mapInputLines, initialBeam);
+
+    while (contraptionMap.GetExistingBeams().Count > 0)
+      contraptionMap.MoveNextAllBeams();
+
+    return contraptionMap.EnergizedTilesCount();
   }
 
 }
